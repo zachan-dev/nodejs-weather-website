eaa114fd126f2f74137b3764a7296302e3e9be54
Zach Chan <chansiuhim@gmail.com>
2020-05-16 11:33:36 +0800

    Add humidity info

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -12,7 +12,9 @@ const forecast = (lat, long, callback) => {
             callback('WeatherStack:', body.error.info, undefined);
         }
         else {
-            callback(undefined, `${body.current.weather_descriptions[0]}. It is currently ${body.current.temperature} degrees out. It feels like ${body.current.feelslike} degrees out.`);
+            callback(undefined, `${body.current.weather_descriptions[0]}. 
+            It is currently ${body.current.temperature} degrees out. It feels like ${body.current.feelslike} degrees out. 
+            The humidity is ${body.current.humidity}%.`);
             //console.log(`${body.current.weather_descriptions[0]}. It is currently ${body.current.temperature} degrees out. It feels like ${body.current.feelslike} degrees out.`);
         }
     });
